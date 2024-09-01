a3e90de4c7619897ab644118bb6ff697bcfd5536
ojasiiitd <ojasva18352@iiitd.ac.in>
2024-09-02 00:05:20 +0530

    added input logic

--- cdl_pipeline/routes.py
+++ cdl_pipeline/routes.py
@@ -1,7 +1,7 @@
 from cdl_pipeline import app
 from cdl_pipeline.forms import DataInputForm
 
-from flask import render_template , flash , redirect , url_for , request , jsonify
+from flask import render_template , flash , redirect , url_for , request , jsonify , session
 
 import pandas as pd
 
@@ -15,17 +15,20 @@ def homepage():
 def inputData():
     form = DataInputForm(request.form)
 
-    df_raw = pd.read_csv("data/Assignment Task _ Dataset.csv" , index_col=False)
+    df_raw = pd.read_csv('data/Assignment Task _ Dataset.csv' , index_col=False)
     df_sample = df_raw.sample(7)
 
     if request.method == "POST" and form.validate():
-        print(form.data)
+        session['op1'] = form.getOp1.data
+        session['op2'] = form.getOp2.data
+        session['data'] = df_raw.to_json(orient='split')
+        return redirect(url_for('report'))
 
     return render_template('input.html',
                            df_html = df_sample.to_html(classes='table table-striped table-bordered'),
                            form = form)
 
 @app.route("/report")
-# user proivides the sequential input using a form here, and also sees the dataset
-def output():
+# generates the report and uses the library created by us
+def report():
     return render_template('report.html')
